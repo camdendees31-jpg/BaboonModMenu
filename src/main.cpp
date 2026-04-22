3eb7452778db5993ae43037722dee4b8477f0605
camdendees31-jpg <camdendees31@gmail.com>
2026-04-21 23:28:28 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,13 @@
 #include "scotland2/shared/modloader.h"
 #include "beatsaber-hook/shared/utils/hooking.hpp"
 #include "beatsaber-hook/shared/utils/logging.hpp"
+#include "beatsaber-hook/shared/utils/il2cpp-utils.hpp"
 
 static modloader::ModInfo modInfo{MOD_ID, VERSION, 0};
 
 bool flyEnabled = false;
-bool speedEnabled = false;
 bool noclipEnabled = false;
+bool speedEnabled = false;
 bool highJumpEnabled = false;
 
 Configuration& getConfig() {
@@ -15,15 +16,22 @@ Configuration& getConfig() {
     return config;
 }
 
+// Hook the main update loop
+MAKE_HOOK_FIND_CLASS_UNSAFE_STATIC(SceneManager_LoadScene, "UnityEngine.SceneManagement", "SceneManager", "LoadScene", void, StringW sceneName) {
+    PaperLogger.info("Scene loading: {}", static_cast<std::string>(sceneName));
+    SceneManager_LoadScene(sceneName);
+}
+
 MOD_EXTERN_FUNC void setup(CModInfo* info) noexcept {
     *info = modInfo.to_c();
     getConfig().Load();
     Paper::Logger::RegisterFileContextId(PaperLogger.tag);
-    PaperLogger.info("Baboon Mod Menu loaded!");
+    PaperLogger.info("Baboon Mod Menu v1.0.0 loaded!");
 }
 
 MOD_EXTERN_FUNC void late_load() noexcept {
     il2cpp_functions::Init();
     PaperLogger.info("Installing hooks...");
+    INSTALL_HOOK(PaperLogger, SceneManager_LoadScene);
     PaperLogger.info("All hooks installed!");
 }
